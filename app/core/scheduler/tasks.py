ea7a966e3ea61bd10f4ae1d8603db90eeb63bda9
apeming <apeaming@gmail.com>
2025-09-09 17:53:02 +0800

    启用自动抢购

--- app/core/scheduler/tasks.py
+++ app/core/scheduler/tasks.py
@@ -4,7 +4,7 @@ import logging
 from typing import Dict, Any
 
 from app.core.config import settings
-from app.models.order import OrderRequest, OrderResponse, ErrorResponse, OrderType
+from app.models.order import OrderRequest, OrderType
 from app.services.order_service_wrapper import order_service_wrapper
 
 logger = logging.getLogger(__name__)
@@ -24,42 +24,50 @@ class TaskRegistry:
 
     @staticmethod
     async def _fetch_market(filter):
+        filter_category_no = filter['categoryNo']
+
         try:
             markets = await order_service_wrapper.get_markets(filter, settings.private_key)
             other_markets = [
                 market for market in markets
-                if filter['categoryNo'] != 0
-                and market['category_no'] != filter['categoryNo']
+                if filter_category_no != 0
+                and market['category_no'] != filter_category_no
             ]
 
             if (len(other_markets) != 0 or len(markets) > 50):
-                logger.warning('获取错误')
+                logger.warning(f'新市场{filter_category_no}获取错误')
                 return
 
-            logger.info(f"发现{len(markets)}个{filter['categoryNo']}新市场")
+            logger.info(f"发现{len(markets)}个{filter_category_no}新市场")
             for market in markets:
                 nft_token_id = market['token_id']
                 price_wei = market['price_wei']
                 created_at_ts = market['created_at_ts']
 
-                logger.info(f'{nft_token_id:}, {price_wei:}, {created_at_ts:}')
+                request = OrderRequest(
+                    nft_token_id=nft_token_id,
+                    order_type=OrderType.MARKET,
+                    price_wei=price_wei,
+                    created_at_ts=created_at_ts,
+                    confirm_real_order=True,
+                    use_proxy=True,
+                    timeout=settings.request_timeout,
+                )
 
-                # request = OrderRequest(
-                #     nft_token_id=nft_token_id,
-                #     order_type='market',
-                #     price_wei=price_wei,
-                #     created_at_ts=created_at_ts,
-                #     confirm_real_order=True,
-                #     use_proxy=True,
-                #     timeout=settings.request_timeout,
-                # )
+                result = await order_service_wrapper.place_order(
+                    request=request,
+                    private_key=settings.private_key
+                )
 
-                # order_service_wrapper.place_order(
-                #     request=request,
-                #     private_key=settings.private_key
-                # )
+                if result.success:
+                    logger.info(f'新市场 {nft_token_id} 抢购完成')
+                else:
+                    logger.error(f'新市场 {nft_token_id} 抢购失败')
+
+                # 如果有多个商品，当前仅抢购第一个
+                break
         except Exception as e:
-            logger.error(f'❌ 获取新市场失败: {e}')
+            logger.error(f'❌ 新市场获取或抢购失败: {e}')
 
     @staticmethod
     async def get_new_markets():
@@ -86,7 +94,6 @@ class TaskRegistry:
                 },
             ]
             await asyncio.gather(*(TaskRegistry._fetch_market(f) for f in filters))
-
         except:
             logger.error("新市场发现失败...")
 
